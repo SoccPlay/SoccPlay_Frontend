c5f78498eb2f8bf337d89c1fb14e06b34b72dc1d
DinhB <dinhb@splus-software.com.vn>
2023-08-11 22:33:15 +0700

    add firstCode

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import Footer from "./components/footer/Footer";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Recommend from "./components/recommend/Recommend";
+import ScrollToTop from "./components/scrollToTop/ScrollToTop";
+import Services from "./components/services/Services";
+import Testimonials from "./components/testimonials/Testimonials";
+import scrollreveal from "scrollreveal";
+export default function App() {
+    useEffect(() => {
+        const sr = scrollreveal({
+            origin: "top",
+            distance: "80px",
+            duration: 2000,
+            reset: true,
+        });
+        sr.reveal(
+            `
+        nav,
+        #hero,
+        #services,
+        #recommend,
+        #testimonials,
+        footer
+        `,
+            {
+                opacity: 0,
+                interval: 300,
+            }
+        );
+    }, []);
+    return (
+        <div>
+            <ScrollToTop />
+            <Navbar />
+            <Hero />
+            <Services />
+            <Recommend />
+            <Testimonials />
+            <Footer />
+        </div>
+    );
+}
