be5865bb9aae0ac1155c1d5ef2a44c67de4e4ad3
magnus <dinhbach3062001@gmail.com>
2023-08-20 18:05:56 +0700

    fix api

--- src/components/Axios/BookingApi.js
+++ src/components/Axios/BookingApi.js
@@ -1,31 +1,34 @@
 import axiosApi from "./AxiosApi";
 
 const BookingApi = {
-  CreateBooking: async (form) => {
-    const url = `Booking/CreateBooking_v3`;
-    return axiosApi.post(url, form);
-  },
+    CreateBooking: async (form) => {
+        const url = `Booking/CreateBooking_v3`;
+        return axiosApi.post(url, form);
+    },
 };
 
-export const getAllBooking = async (customerId) => {
-  try {
-    const response = await axiosApi.get("/Booking/GetAllBookingByCustomerId", {
-      params: {
-        customerId,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.log("Failed to fetch schedule: ", error);
-  }
+export const getAllBooking = async (id) => {
+    try {
+        const response = await axiosApi.get(
+            "/Booking/GetAllBookingByCustomerId",
+            {
+                params: {
+                    id,
+                },
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.log("Failed to fetch schedule: ", error);
+    }
 };
 
 export const deleteBooking = async (id) => {
-  try {
-    await axiosApi.delete(`/Booking/CancelBooking?id=${id}`);
-  } catch (error) {
-    console.log("Failed to fetch schedule: ", error);
-  }
+    try {
+        await axiosApi.delete(`/Booking/CancelBooking?id=${id}`);
+    } catch (error) {
+        console.log("Failed to fetch schedule: ", error);
+    }
 };
 
 export default BookingApi;
