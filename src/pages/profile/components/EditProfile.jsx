cb3a0da342fa077e214883c1628b71038d55f58d
magnus <dinhbach3062001@gmail.com>
2023-08-21 14:30:29 +0700

    refactor code

--- src/pages/profile/components/EditProfile.jsx
+++ src/pages/profile/components/EditProfile.jsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from "react";
 import CustomerApi from "../../../components/Axios/CustomerApi";
 import * as Yup from "yup";
 import { useFormik } from "formik";
+import { withSnackbar } from "../../../hook/withSnackbar";
 
-export const EditProfile = ({ onClosed }) => {
+const EditProfile = ({ snackbarShowMessage }) => {
     const [profile, setProfile] = useState([]);
 
     const validationSchema = Yup.object().shape({
         email: Yup.string()
-            .email("Email không hợp lệ")
+            .matches(/[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Email không hợp lệ")
             .required("Vui lòng nhập email"),
         phone: Yup.string()
             .matches(
@@ -34,11 +35,13 @@ export const EditProfile = ({ onClosed }) => {
         enableReinitialize: true,
         validationSchema: validationSchema,
         onSubmit: async (values) => {
-            console.log("values", values);
-            await CustomerApi.updateProfile(
-                localStorage.getItem("ACCOUNTID"),
-                values
-            );
+            try {
+                await CustomerApi.updateProfile(values);
+                snackbarShowMessage("Cập nhật thông tin thành công", "success");
+            } catch (error) {
+                console.log("Error", error.response.data.Messages);
+                snackbarShowMessage("Cập nhật thông tin thất bại", "error");
+            }
         },
     });
     console.log("profile", profile);
@@ -57,151 +60,156 @@ export const EditProfile = ({ onClosed }) => {
                 height: "100vh",
             }}
         >
-            <Grid item xs={12}>
-                <Typography
-                    fontSize={"30px"}
-                    fontWeight={"bold"}
-                    color={"black"}
-                >
-                    Cập nhật thông tin cá nhân
-                </Typography>
-            </Grid>
-            <form onSubmit={formik.handleSubmit}></form>
-            <Grid
-                item
-                container
-                justifyContent={"space-between"}
-                xs={12}
-                mt={"40px"}
-            >
+            <form onSubmit={formik.handleSubmit}>
                 <Grid item xs={12}>
-                    <Grid item xs={12} mb={"8px"}>
-                        <TitleTextField fontSize={"24px"}>
-                            Họ và tên
-                        </TitleTextField>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            value={formik.values.fullName}
-                            fullWidth
-                            name="fullName"
-                            type="text"
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                        />
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid
-                item
-                container
-                justifyContent={"space-between"}
-                xs={12}
-                mt={"10px"}
-            >
-                <Grid item xs={12}>
-                    <Grid item xs={12} mb={"8px"}>
-                        <TitleTextField fontSize={"23px"}>Email</TitleTextField>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            value={formik.values.email}
-                            fullWidth
-                            name="email"
-                            type="email"
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                        />
-                        {formik.touched.email && formik.errors.email ? (
-                            <div style={{ color: "red" }}>
-                                {formik.errors.email}
-                            </div>
-                        ) : null}
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid
-                item
-                container
-                justifyContent={"space-between"}
-                xs={12}
-                mt={"10px"}
-            >
-                <Grid item xs={12}>
-                    <Grid item xs={12} mb={"8px"}>
-                        <TitleTextField fontSize={"23px"}>
-                            Địa chỉ
-                        </TitleTextField>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            value={formik.values.address}
-                            fullWidth
-                            name="address"
-                            type="text"
-                            placeholder="Số nhà, đường, phường, quận, thành phố"
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                        />
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid
-                item
-                container
-                justifyContent={"space-between"}
-                xs={12}
-                mt={"10px"}
-            >
-                <Grid item xs={12}>
-                    <Grid item xs={12} mb={"8px"}>
-                        <TitleTextField fontSize={"23px"}>
-                            Số điện thoại
-                        </TitleTextField>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            value={formik.values.phone}
-                            fullWidth
-                            name="phone"
-                            type="text"
-                            placeholder="0123-456-789"
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                        />
-                        {formik.touched.phone && formik.errors.phone ? (
-                            <div style={{ color: "red" }}>
-                                {formik.errors.phone}
-                            </div>
-                        ) : null}
-                    </Grid>
-                </Grid>
-            </Grid>
-            <Grid
-                item
-                container
-                justifyContent={"space-between"}
-                xs={12}
-                mt={"10px"}
-            ></Grid>
-            <Grid item container xs={12} mt={"40px"}>
-                <Grid item xs={"auto"}>
-                    <Button
-                        variant="contained"
-                        sx={{
-                            border: "2px solid #1976d2",
-                            marginBottom: "40px",
-                        }}
-                        onClick={formik.handleSubmit}
-                        disabled={formik.isSubmitting}
+                    <Typography
+                        fontSize={"30px"}
+                        fontWeight={"bold"}
+                        color={"black"}
                     >
-                        Lưu
-                    </Button>
+                        Cập nhật thông tin cá nhân
+                    </Typography>
                 </Grid>
-            </Grid>
+                <Grid
+                    item
+                    container
+                    justifyContent={"space-between"}
+                    xs={12}
+                    mt={"40px"}
+                >
+                    <Grid item xs={12}>
+                        <Grid item xs={12} mb={"8px"}>
+                            <TitleTextField fontSize={"24px"}>
+                                Họ và tên
+                            </TitleTextField>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                value={formik.values.fullName}
+                                fullWidth
+                                name="fullName"
+                                type="text"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid
+                    item
+                    container
+                    justifyContent={"space-between"}
+                    xs={12}
+                    mt={"10px"}
+                >
+                    <Grid item xs={12}>
+                        <Grid item xs={12} mb={"8px"}>
+                            <TitleTextField fontSize={"23px"}>
+                                Email
+                            </TitleTextField>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                value={formik.values.email}
+                                fullWidth
+                                name="email"
+                                type="email"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {formik.touched.email && formik.errors.email ? (
+                                <div style={{ color: "red" }}>
+                                    {formik.errors.email}
+                                </div>
+                            ) : null}
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid
+                    item
+                    container
+                    justifyContent={"space-between"}
+                    xs={12}
+                    mt={"10px"}
+                >
+                    <Grid item xs={12}>
+                        <Grid item xs={12} mb={"8px"}>
+                            <TitleTextField fontSize={"23px"}>
+                                Địa chỉ
+                            </TitleTextField>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                value={formik.values.address}
+                                fullWidth
+                                name="address"
+                                type="text"
+                                placeholder="Số nhà, đường, phường, quận, thành phố"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid
+                    item
+                    container
+                    justifyContent={"space-between"}
+                    xs={12}
+                    mt={"10px"}
+                >
+                    <Grid item xs={12}>
+                        <Grid item xs={12} mb={"8px"}>
+                            <TitleTextField fontSize={"23px"}>
+                                Số điện thoại
+                            </TitleTextField>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                value={formik.values.phone}
+                                fullWidth
+                                name="phone"
+                                type="text"
+                                placeholder="0123-456-789"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                            {formik.touched.phone && formik.errors.phone ? (
+                                <div style={{ color: "red" }}>
+                                    {formik.errors.phone}
+                                </div>
+                            ) : null}
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid
+                    item
+                    container
+                    justifyContent={"space-between"}
+                    xs={12}
+                    mt={"10px"}
+                ></Grid>
+                <Grid item container xs={12} mt={"40px"}>
+                    <Grid item xs={"auto"}>
+                        <Button
+                            variant="contained"
+                            sx={{
+                                border: "2px solid #1976d2",
+                                marginBottom: "40px",
+                            }}
+                            type="submit"
+                            disabled={formik.isSubmitting}
+                        >
+                            Lưu
+                        </Button>
+                    </Grid>
+                </Grid>
+            </form>
         </Grid>
     );
 };
+
+export default withSnackbar(EditProfile);
 
 const TitleTextField = styled(Typography)({
     fontSize: "26px",
